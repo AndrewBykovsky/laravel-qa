6a6e37eaae509542420c7eb49e0f6cde9e18c71b
Dmitriy Nilolenko <dnikolenko@mooseworldwidedigital.com>
2020-07-06 14:04:59 +0300

    Added set new answers form

--- app/Answer.php
+++ app/Answer.php
@@ -5,7 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
-{
+{	
+
+	protected $fillable = ['body', 'user_id'] ;
+
     public function question(){
     	return $this->belongsTo(Question::class) ;
     }
